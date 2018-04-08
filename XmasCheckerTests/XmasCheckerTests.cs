cee38dda6bf54a72d6e6d946db4a3f1658fbafd9
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-08 10:09:15 +0800

    prepare project

---
+++ XmasCheckerTests/XmasCheckerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XmasChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmasChecker.Tests
+{
+    [TestClass()]
+    public class XmasCheckerTests
+    {
+        [TestMethod()]
+        public void IsTodayXmasTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
