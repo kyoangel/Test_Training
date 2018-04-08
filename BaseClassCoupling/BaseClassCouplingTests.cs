3eea34834264848180aff8265bbe84a37defa920
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-08 12:28:13 +0800

    complete BaseClassCoupling test

--- BaseClassCoupling/BaseClassCouplingTests.cs
+++ BaseClassCoupling/BaseClassCouplingTests.cs
@@ -6,32 +6,62 @@ namespace BaseClassCoupling
     [TestClass]
     public class BaseClassCouplingTests
     {
+        /// <summary>
+        /// if my monthly salary is 1200, working year is 0.5, my bonus should be 600
+        /// </summary>
         [TestMethod]
         public void calculate_half_year_employee_bonus()
         {
-            //if my monthly salary is 1200, working year is 0.5, my bonus should be 600
-            var lessThanOneYearEmployee = new LessThanOneYearEmployee()
+            var lessThanOneYearEmployee = new FakeEmployee()
             {
                 Id = 91,
-                //Console.WriteLine("your StartDate should be :{0}", DateTime.Today.AddDays(365/2*-1));
                 Today = new DateTime(2018, 1, 27),
                 StartWorkingDate = new DateTime(2017, 7, 29)
             };
-
+            lessThanOneYearEmployee.Log = new TestConsoleLogAdapter();
             var actual = lessThanOneYearEmployee.GetYearlyBonus();
             Assert.AreEqual(600, actual);
         }
     }
 
+    public class FakeEmployee : LessThanOneYearEmployee
+    {
+        protected override decimal GetMonthlySalary()
+        {
+            return 1200;
+        }
+    }
+
+    public class TestConsoleLogAdapter : ILog
+    {
+        public void Info(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
     public abstract class Employee
     {
+        private ILog _log;
         public DateTime StartWorkingDate { get; set; }
         public DateTime Today { get; set; }
 
-        protected decimal GetMonthlySalary()
+        protected virtual decimal GetMonthlySalary()
         {
-            DebugHelper.Info($"query monthly salary id:{Id}");
+            Log.Info($"query monthly salary id:{Id}");
             return SalaryRepo.Get(this.Id);
+        }
+
+        public ILog Log
+        {
+            get
+            {
+                return _log ?? new DebugHelperLogAdapter();
+            }
+            set
+            {
+                _log = value;
+            }
         }
 
         public abstract decimal GetYearlyBonus();
@@ -39,19 +69,33 @@ namespace BaseClassCoupling
         public int Id { get; set; }
     }
 
+    public interface ILog
+    {
+        void Info(string text);
+    }
+
+    public class DebugHelperLogAdapter : ILog
+    {
+        public void Info(string text)
+        {
+            DebugHelper.Info(text);
+        }
+    }
+
     public class LessThanOneYearEmployee : Employee
     {
         public override decimal GetYearlyBonus()
         {
-            DebugHelper.Info("--get yearly bonus--");
+            Log.Info("--get yearly bonus--");
             var salary = this.GetMonthlySalary();
-            DebugHelper.Info($"id:{Id}, his monthly salary is:{salary}");
+
+            Log.Info($"id:{Id}, his monthly salary is:{salary}");
             return Convert.ToDecimal(this.WorkingYear()) * salary;
         }
 
         private double WorkingYear()
         {
-            DebugHelper.Info("--get working year--");
+            Log.Info("--get working year--");
             var year = (Today - StartWorkingDate).TotalDays / 365;
             return year > 1 ? 1 : Math.Round(year, 2);
         }
