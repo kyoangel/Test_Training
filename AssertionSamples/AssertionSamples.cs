cb0f657fea4fdba8e46ec2e8c869b8b071b76445
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-08 10:52:08 +0800

    prepare AssertionSamples

---
+++ AssertionSamples/AssertionSamples.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace AssertionSamples
+{
+    [TestClass]
+    public class AssertionSamples
+    {
+        private readonly CustomerRepo _customerRepo = new CustomerRepo();
+
+        [TestMethod]
+        public void CompareCustomer()
+        {
+            var actual = _customerRepo.Get();
+            var expected = new Customer
+            {
+                Id = 2,
+                Age = 18,
+                Birthday = new DateTime(1990, 1, 26)
+            };
+
+            //how to assert customer?
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CompareCustomerList()
+        {
+            var actual = _customerRepo.GetAll();
+
+            var expected = new List<Customer>
+            {
+                new Customer()
+                {
+                    Id=3,
+                    Age=20,
+                    Birthday = new DateTime(1993,1,2)
+                },
+
+                new Customer()
+                {
+                    Id=4,
+                    Age=21,
+                    Birthday = new DateTime(1993,1,3)
+                },
+            };
+
+            //how to assert customers?
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CompareComposedCustomer()
+        {
+            var actual = _customerRepo.GetComposedCustomer();
+            var expected = new Customer()
+            {
+                Age = 30,
+                Id = 11,
+                Birthday = new DateTime(1999, 9, 9),
+                Order = new Order { Id = 19, Price = 91 },
+            };
+
+            //how to assert composed customer?
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Partials the compare customer birthday and order price.
+        /// partial compare should use anonymous type
+        /// </summary>
+        [TestMethod]
+        public void PartialCompare_Customer_Birthday_And_Order_Price()
+        {
+            var actual = _customerRepo.GetComposedCustomer();
+
+            var expected = new
+            {
+                Birthday = new DateTime(1999, 9, 9),
+                Order = new { Price = 91 },
+            };
+
+            //how to assert actual is equal to expected?
+            Assert.Fail();
+        }
+    }
+
+    public class CustomerRepo
+    {
+        public Customer Get()
+        {
+            return new Customer
+            {
+                Id = 2,
+                Age = 18,
+                Birthday = new DateTime(1990, 1, 26)
+            };
+        }
+
+        public List<Customer> GetAll()
+        {
+            return new List<Customer>
+            {
+                new Customer()
+                {
+                    Id=3,
+                    Age=20,
+                    Birthday = new DateTime(1993,1,2)
+                },
+
+                new Customer()
+                {
+                    Id=4,
+                    Age=21,
+                    Birthday = new DateTime(1993,1,3)
+                },
+            };
+        }
+
+        public Customer GetComposedCustomer()
+        {
+            return new Customer()
+            {
+                Age = 30,
+                Id = 11,
+                Birthday = new DateTime(1999, 9, 9),
+                Order = new Order { Id = 19, Price = 91 },
+            };
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public Order Order { get; set; }
+    }
+}
---
+++ AssertionSamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssertionSamples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssertionSamples")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c186f4b6-34f6-4ada-8536-df0563c99d12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
