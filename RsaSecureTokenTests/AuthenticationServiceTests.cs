515026944d4ff348acfd869a95c38e9d3b9471bf
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-08 15:48:59 +0800

    add RsaSecureToken Project for sum up

---
+++ RsaSecureTokenTests/AuthenticationServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RsaSecureToken;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RsaSecureToken.Tests
+{
+    [TestClass()]
+    public class AuthenticationServiceTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            var target = new AuthenticationService();
+
+            var actual = target.IsValid("joey", "91000000");
+
+            //always failed
+            Assert.IsTrue(actual);
+        }
+    }
+}
