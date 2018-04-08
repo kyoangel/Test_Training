27e2f1675c2ff8d36c96ede2fa96ed66a9eb5783
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-08 10:29:43 +0800

    complete test for today is xmas

--- XmasChecker/XmasChecker.cs
+++ XmasChecker/XmasChecker.cs
@@ -7,12 +7,17 @@ namespace XmasChecker
         public bool IsTodayXmas()
         {
             // implement
-            var today = DateTime.Today;
+            var today = GetToday();
             if (today.Month == 12 && today.Day == 25)
             {
                 return true;
             }
             return false;
+        }
+
+        internal virtual DateTime GetToday()
+        {
+            return DateTime.Today;
         }
     }
 }
