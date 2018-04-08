1f58a5835fa5fc71bdc2ad695d517b5796faaf9f
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-08 10:20:55 +0800

    complete production code

--- XmasChecker/XmasChecker.cs
+++ XmasChecker/XmasChecker.cs
@@ -1,10 +1,17 @@
-﻿namespace XmasChecker
+﻿using System;
+
+namespace XmasChecker
 {
     public class XmasChecker
     {
         public bool IsTodayXmas()
         {
             // implement
+            var today = DateTime.Today;
+            if (today.Month == 12 && today.Day == 25)
+            {
+                return true;
+            }
             return false;
         }
     }
