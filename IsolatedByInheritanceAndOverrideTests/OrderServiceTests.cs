388c37507f8cd6a3b9f2c1612d5b51c7d5eddf27
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-08 15:25:09 +0800

    prepare IsolatedByInheritanceAndOverride Projects

---
+++ IsolatedByInheritanceAndOverrideTests/OrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IsolatedByInheritanceAndOverride;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsolatedByInheritanceAndOverride.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        /// <summary>
+        /// Tests the synchronize book orders 3 orders only 2 book order.
+        /// </summary>
+        [TestMethod()]
+        public void Test_SyncBookOrders_3_Orders_Only_2_book_order()
+        {
+            //Try to isolate dependency to unit test
+
+            //var target = new OrderService();
+            //target.SyncBookOrders();
+            Assert.Fail();
+        }
+    }
+}
