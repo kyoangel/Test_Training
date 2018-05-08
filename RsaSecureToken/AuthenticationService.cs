ef5431bbe14b905f81d88d89b4cab0640eb6f81a
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-05-08 22:02:22 +0800

    use utcnow.substract for better meaning

--- RsaSecureToken/AuthenticationService.cs
+++ RsaSecureToken/AuthenticationService.cs
@@ -33,6 +33,7 @@ namespace RsaSecureToken
     {
         public int GetRegisterTimeInMinutes(string account)
         {
+            // Not Complete yet
             throw new NotImplementedException();
         }
     }
@@ -41,9 +42,8 @@ namespace RsaSecureToken
     {
         public string GetRandom(int minutes)
         {
-            var seed = new Random((int)DateTimeOffset.Now.ToUnixTimeSeconds()/60 - minutes);
+            var seed = new Random((int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMinutes - minutes);
             var result = seed.Next(0, 999999).ToString("000000");
-            Console.WriteLine("randomCode:{0}", result);
 
             return result;
         }
