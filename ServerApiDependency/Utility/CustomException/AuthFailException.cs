421b1800a6f84e2e1c19e140fb2167ad16e534ea
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-05-16 22:10:13 +0800

    make practice project more readable

---
+++ ServerApiDependency/Utility/CustomException/AuthFailException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ServerApiDependency.Utility.CustomException
+{
+    public class AuthFailException : Exception
+    {
+        public AuthFailException()
+        {
+            
+        }
+
+        public AuthFailException(string message) : base(message)
+        {
+        }
+
+        public AuthFailException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AuthFailException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
