f80e745700b13df0e031320dfacd7f2397aeb8dd
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-05-17 21:01:44 +0800

    add implement for xmaschecker to speed up training

--- XmasChecker/XmasChecker.cs
+++ XmasChecker/XmasChecker.cs
@@ -1,10 +1,16 @@
-﻿namespace XmasChecker
+﻿using System;
+
+namespace XmasChecker
 {
     public class XmasChecker
     {
         public bool IsTodayXmas()
         {
-            // implement
+            var today = DateTime.Today;
+            if (today.Month == 12 && today.Day == 25)
+            {
+                return true;
+            }
             return false;
         }
     }
